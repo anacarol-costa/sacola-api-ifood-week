4379fdfe880002ef66aab68ad2bf23f4ec267ed3
anacarol.costa <carol.costa2502@gmail.com>
2022-10-02 11:41:29 -0300

    Spring initializr

---
+++ src/main/java/anaCarolCosta/dio/sacolaiFood/SacolaApiIfoodWeekDioApplication.java
@@ -0,0 +1,13 @@
+package anaCarolCosta.dio.sacolaiFood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SacolaApiIfoodWeekDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SacolaApiIfoodWeekDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/anaCarolCosta/dio/sacolaiFood/SacolaApiIfoodWeekDioApplicationTests.java
@@ -0,0 +1,13 @@
+package anaCarolCosta.dio.sacolaiFood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SacolaApiIfoodWeekDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
